1ebfa5216d1f2e0c5aca57dce42e6fde31a55ff3
Devendra Parkar <devendra.parkar@asu.edu>
2023-01-19 13:54:01 -0700

    Added scaling and percentage mutator

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SwarmAggregationGA
 This is a research project in Swarm robotics using Genetic algorithm as the backbone to search for local controllers to produce a global aggregation behaviour. The base model used is the SOPS model from collective research lab at ASU.
+
+Account for diversity of the a genome wrt to other genomes into its fitness ??
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
-use rand::{distributions::Uniform, rngs, Rng};
+use gag::Redirect;
+use rand::{distributions::Bernoulli, distributions::Open01, distributions::Uniform, rngs, Rng};
 use rand_distr::Normal;
 use rayon::prelude::*;
 use std::convert::TryInto;
+use std::time::Instant;
+// use std::env;
+use std::fs::OpenOptions;
+use std::io::{Read, Seek, SeekFrom, Write};
+// use std::path::Path;
+// use std::fs::File;
+// use std::io::prelude::*;
+// use chrono::prelude::*;
+
+// const EXP: &'static str = "1";
+// const ROOT_PATH: &'static str = "/output/";
+
+fn get_temp_filepath() -> String {
+    #[cfg(unix)]
+    return "./output/trial.log".into();
+}
 
 struct Particle {
     x: u8,
@@ -65,6 +82,7 @@ impl SOPSEnvironment {
     }
 
     fn static_init(genome: &[f64; 6]) -> Self {
+        // initial starting fitness of given configuration is 154
         let grid: [[bool; 18]; 18] = [
             [
                 true, true, false, true, false, false, false, true, false, false, false, true,
@@ -264,7 +282,7 @@ impl SOPSEnvironment {
 
     fn simulate(&mut self) -> u32 {
         for _ in 0..self.sim_duration {
-            self.move_particles(4);
+            self.move_particles((self.participants.len() as f32 * 0.03) as usize);
         }
         let fitness = self.evaluate_fitness();
         self.fitness_val = fitness as f64;
@@ -274,8 +292,9 @@ impl SOPSEnvironment {
 
 impl GeneticAlgo {
     #[inline]
-    fn genome_init_rng() -> Normal<f64> {
-        Normal::new(0.5, 0.1).unwrap()
+    fn genome_init_rng() -> Open01 {
+        // Normal::new(0.5, 0.1).unwrap()
+        Open01
     }
 
     #[inline]
@@ -289,6 +308,10 @@ impl GeneticAlgo {
 
     fn cross_pnt() -> Uniform<u16> {
         Uniform::new_inclusive(0, 5)
+    }
+
+    fn mut_sign() -> Bernoulli {
+        Bernoulli::new(0.2).unwrap()
     }
 
     #[inline]
@@ -334,21 +357,27 @@ impl GeneticAlgo {
         for i in 0..genome.len() {
             let smpl = SOPSEnvironment::rng().sample(&GeneticAlgo::unfrm_100());
             if smpl as f64 <= self.mut_rate * 100.0 {
-                let perturb = SOPSEnvironment::rng().sample(self.mut_val());
-                new_genome[i] = (genome[i] + perturb).clamp(0.0, 1.0);
+                // let perturb = SOPSEnvironment::rng().sample(self.mut_val());
+                let per_dir = SOPSEnvironment::rng().sample(&GeneticAlgo::mut_sign());
+                let scale = if per_dir { 10.0 } else { 0.1 };
+                new_genome[i] = (scale * genome[i]).clamp(0.0, 1.0);
+                // let scale = if per_dir { 0.1 } else { -0.1 };
+                // print!("{y:.5?} ",y=scale*genome[i]);
+                // new_genome[i] = (genome[i] + scale*genome[i]).clamp(0.0, 1.0);
             }
+            // print!("\t");
         }
+        // println!("");
         new_genome
     }
 
     fn generate_offspring(&self, parent1: &[f64; 6], parent2: &[f64; 6]) -> [f64; 6] {
-        let mut new_genome:[f64; 6] = [0.0; 6];
+        let mut new_genome: [f64; 6] = [0.0; 6];
         let cross_pnt = SOPSEnvironment::rng().sample(&GeneticAlgo::cross_pnt());
         for i in 0..new_genome.len() {
             if i as u16 <= cross_pnt {
                 new_genome[i] = parent1[i];
-            }
-            else {
+            } else {
                 new_genome[i] = parent2[i];
             }
         }
@@ -359,11 +388,21 @@ impl GeneticAlgo {
         let mut new_pop: Vec<Genome> = vec![];
         let mut selected_g: Vec<[f64; 6]> = vec![];
         let mut rank_wheel: Vec<usize> = vec![];
+        //sort the genomes in population by fitness value
         self.population.sort_unstable_by(|genome_a, genome_b| {
             genome_b.fitness.partial_cmp(&genome_a.fitness).unwrap()
         });
+        // /*
+        //print genomes for analysis
+        let best_genome = self.population[0];
+        println!("Best Genome -> {best_genome:.5?}");
 
-        //bifercate genomes 
+        for idx in 1..self.population.len() {
+            println!("{y:.5?}", y = self.population[idx]);
+        }
+        // */
+        // /*
+        //bifercate genomes
         for (index, genome) in self.population.iter().enumerate() {
             if index < self.elitist_cnt as usize {
                 //separate out the elitist and directly pass them to next gen
@@ -382,12 +421,31 @@ impl GeneticAlgo {
             let p_genome_idx1 = rank_wheel[wheel_idx];
             wheel_idx = SOPSEnvironment::rng().sample(&rank_wheel_rng);
             let p_genome_idx2 = rank_wheel[wheel_idx];
-            selected_g.push(self.generate_offspring(&self.population[p_genome_idx1].string, &self.population[p_genome_idx2].string));
-            // selected_g.push(self.population[p_genome_idx1].string);
+            selected_g.push(self.generate_offspring(
+                &self.population[p_genome_idx1].string,
+                &self.population[p_genome_idx2].string,
+            ));
         }
+        // */
+        /*
+        //dummy pass
+        for idx in 0..(self.population.len()) {
+            // let mut wheel_idx = SOPSEnvironment::rng().sample(&rank_wheel_rng);
+            // let p_genome_idx1 = rank_wheel[wheel_idx];
+            // wheel_idx = SOPSEnvironment::rng().sample(&rank_wheel_rng);
+            // let p_genome_idx2 = rank_wheel[wheel_idx];
+            // selected_g.push(self.generate_offspring(
+            //     &self.population[p_genome_idx1].string,
+            //     &self.population[p_genome_idx2].string,
+            // ));
+            selected_g.push(self.population[idx].string); // added
+        }
+        */
+
         //perform mutation
         for idx in 0..selected_g.len() {
             let genome = selected_g[idx];
+            // println!("Genome:{} mutations", idx);
             let mutated_g = self.mutate_genome(&genome);
             new_pop.push(Genome {
                 string: mutated_g,
@@ -406,16 +464,23 @@ impl GeneticAlgo {
                 return;
             }
             let genome_s = genome.string.clone();
-            let mut fitness_t = 0.0;
-            for _ in 0..trials {
+            let fitness_t: f64 = (0..trials).into_par_iter().fold(|| 0_f64, |sum, _| {
                 let mut genome_env = SOPSEnvironment::static_init(&genome_s);
                 let g_fitness = genome_env.simulate();
-                fitness_t += g_fitness as f64;
-            }
+                // let g_fitness = 1; // added
+                sum + g_fitness as f64
+            }).sum();
+            // let mut fitness_t = 0.0;
+            // for _ in 0..trials {
+            //     // let mut genome_env = SOPSEnvironment::static_init(&genome_s);
+            //     // let g_fitness = genome_env.simulate();
+            //     let g_fitness: f64 = genome_s.iter().sum(); // added
+            //     fitness_t += g_fitness;
+            // }
 
             genome.fitness = fitness_t / (trials as f64) as f64;
         });
-        //avg.fitness of old population
+        //avg.fitness of population
         let fit_sum = self
             .population
             .iter()
@@ -424,6 +489,7 @@ impl GeneticAlgo {
             "Avg. Fitness -> {}",
             fit_sum / (self.population.len() as f64)
         );
+        //calculate population diversity
         let mut pop_dist: Vec<f64> = vec![];
         for i in 0..self.population.len() {
             for j in (i + 1)..self.population.len() {
@@ -444,59 +510,109 @@ impl GeneticAlgo {
         );
         //generate new population
         self.generate_new_pop();
-        let best_genome = self.population[0];
-        println!("Best Genome -> {best_genome:?}");
     }
 
     fn run_through(&mut self) {
+        // let utc: DateTime<Utc> = Utc::now();
+        // let file_path = format!("{ROOT_PATH}exp_{dt}",dt = utc.format("%d_%m_%Y_%H:%M"));
+        // let mut file = File::create(file_path)?;
         for gen in 0..self.max_gen {
             println!("Starting Gen:{}", gen);
+            let now = Instant::now();
             self.step_through(gen);
+            let elapsed = now.elapsed().as_secs();
+            println!("Elapsed Time: {:.2?}", elapsed);
         }
-        let best_genome = self.population[0];
-        let mut best_genome_env = SOPSEnvironment::static_init(&best_genome.string);
-        best_genome_env.print_grid();
-        let g_fitness = best_genome_env.simulate();
-        best_genome_env.print_grid();
-        println!("Best genome's fitness is {}", g_fitness);
-        println!("{best_genome:?}");
+        // let best_genome = self.population[0];
+        // let mut best_genome_env = SOPSEnvironment::static_init(&best_genome.string);
+        // best_genome_env.print_grid();
+        // let g_fitness = best_genome_env.simulate();
+        // best_genome_env.print_grid();
+        // println!("Best genome's fitness is {}", g_fitness);
+        // println!("{best_genome:?}");
     }
 }
 
 fn main() {
-    let mut ga_sops = GeneticAlgo::init_ga(20, 50, 3, 3, 0.07, 0.0, 0.05);
+    // Open a log
+    let log = OpenOptions::new()
+        .truncate(true)
+        .read(true)
+        .create(true)
+        .write(true)
+        .open(get_temp_filepath())
+        .unwrap();
+
+    let print_redirect = Redirect::stdout(log).unwrap();
+
+    let mut ga_sops = GeneticAlgo::init_ga(50, 50, 1, 1, 0.1, 0.03, 0.01);
     ga_sops.run_through();
+
+    /*
+    // Block for running single experiments
+    // Genome from know solution space -> 307
     // let genome = [
     //     0.879410923312898,
     //     0.7072823455008604,
-    //     0.3758316160483933,
-    //     0.4947528743281018,
-    //     0.32321085020900764,
-    //     0.6018268442926183,
+    //     0.0758316160483933,
+    //     0.0447528743281018,
+    //     0.005321085020900764,
+    //     0.0018268442926183,
     // ];
+    let genome = [
+        0.5994110836839489,
+        0.6183247501358494,
+        0.5522009562182426,
+        0.4959382596880117,
+        0.2769269870929103,
+        0.523770334862512,
+    ];
+    // println!("{:?}", genome);
+
     // let mut sops_trial = SOPSEnvironment::init_sops_env(&genome);
     // sops_trial.print_grid();
     // println!("{}", sops_trial.evaluate_fitness());
     // println!("No. of Participants {}", sops_trial.participants.len());
-    // let mut sops_trial_2 = SOPSEnvironment::static_init(&genome);
-    // sops_trial_2.print_grid();
-    // println!("{}", sops_trial_2.evaluate_fitness());
-    // println!("{}",sops_trial.evaluate_fitness());
-    // println!("{}",sops_trial.simulate());
-    // sops_trial.print_grid();
+    let scores: Vec<u32> = (0..10)
+        .into_iter()
+        .map(|idx| {
+            let mut sops_trial_2 = SOPSEnvironment::static_init(&genome);
+            // sops_trial_2.print_grid();
+            // println!("{}", sops_trial_2.evaluate_fitness());
+            // // println!("{}",sops_trial.evaluate_fitness());
+            // println!("{}", );
+            // sops_trial_2.print_grid();
+            let now = Instant::now();
+            let score = sops_trial_2.simulate();
+            let elapsed = now.elapsed().as_secs();
+            println!("Trial {idx} Elapsed Time: {:.2?}", elapsed);
+            score
+        })
+        .collect();
 
+    let mean = scores.clone().into_iter().fold(0, |sum, score| sum + score) / 1;
+
+    let variance = scores
+        .into_iter()
+        .fold(0, |sum, score| sum + (score as i32 - mean as i32).pow(2))
+        / 1;
+
+    println!("N^3 Mean:{}", mean);
+    println!("N^3 Variance:{}", variance);
+    // scores.for_each(|s| { println!("{}", s);});
+     */
     // let mut grid: [[bool; 18]; 18] = [[false; 18]; 18];
-    // let init_nrm = Normal::new(0.5, 0.1).unwrap();
+    // let init_nrm = Normal::new(0.0, 0.05).unwrap();
+    // let bi = Bernoulli::new(0.5).unwrap();
     // let mut rng = rand::thread_rng();
     // let grid_rng = Uniform::new(0, grid.len());
-
-    // println!("A simple Grid\n");
-
-    // println!("{:?}", genome);
-
-    // println!("{:?}", genome);
-
-    // let v = rng.sample(&init_nrm);
-    // println!("{} is from a N(0.5, 0.1) distribution", v);
+    // let mut cnt = 0;
+    // for _i in 0..1000 {
+    //     let v = rng.sample(&bi);
+    //     // println!("{}", v);
+    //     if v { cnt += 1; }
+    // }
+    // println!("{}",cnt);
     // println!("{} particles in grid of {} vertices",(((grid.len()*grid.len()) as f32)*0.3) as u64, grid.len()*grid.len());
+    print_redirect.into_inner();
 }
